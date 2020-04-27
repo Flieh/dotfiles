301d712430338e16fa393a8498ecbe9c289245b3
flieh <ruedelaflieh@gmail.com>
2020-04-27 17:14:59 +0200

    update

--- links.bash
+++ links.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
-if [ -d $HOME/$1 ] ; then echo "$1 is a directory" ; exit 1 ; fi
-if [ -h $HOME/$1 ] ; then echo "$1 is a symbolic link" ; exit 1 ; fi
-if [ -f $HOME/$1 ] ; then echo "$1 is a regular file" ; exit 1 ; fi
-
+if [ -d $HOME/$1 ]  
+then  
+    echo ""
+else 
+    echo "$1 is not a directory"
+fi
